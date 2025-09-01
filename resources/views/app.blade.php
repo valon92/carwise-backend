4e6121f660418fdfec0af520b280598760605b90
valon92 <121667444+valon92@users.noreply.github.com>
2025-09-01 05:52:59 +0200

    Fix CSRF token and session issues - Removed @routes directive from app.blade.php - Added SESSION_DOMAIN=localhost to .env - Cleared all caches and rebuilt assets - Fixed 419 PAGE EXPIRED error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,6 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @routes
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
     </head>
